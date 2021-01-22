a9cc6653fff402d261c3af74141d4be11c020a1d
vijay466 <ardhanivinay100@gmail.com>
2021-01-22 13:57:46 +0530

    first commit

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+
+app.get('/',(req,res)=> res.send("Hello world")
+);
+app.listen(port,()=> {
+    console.log("Server is up and running")
+});
